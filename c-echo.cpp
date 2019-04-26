8d5e1ed023384ed5e630f56fd9c9aed9f829af24
borninla <borninla@gmail.com>
2019-04-26 13:48:16 -0700

    getting ready for unit testing section

--- c-echo.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main(int argv, char** argc) {
-	for(int i = 1; i < argv; i++) {
-		std::cout << argc[i] << " ";
-	}
-	std::cout << std::endl;
-}
---
+++ c-echo.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argv, char** argc) {
+	// SKip the first argc index because its the program
+	for(int i = 1; i < argv; i++) {
+		std::cout << argc[i];
+		// Print a whitespace after all but the last iteration
+		if (i < argv - 1)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
+}
